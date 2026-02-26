7db097141ceab4e841fa72137d9956c7179af5ff
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-02-26 14:54:18 -0600

    67. Pruebas en HttpClient Adapter

--- 02-bases/src/plugins/http-client.plugin.ts
+++ 02-bases/src/plugins/http-client.plugin.ts
@@ -8,9 +8,15 @@ const httpClientPlugin = {
     const { data } = await axios.get(url);
     return data;
   },
-  // post: async (url, body) => {},
-  // put: async (url, body) => {},
-  // delete: async (url) => {},
+  post: async (url: string, body: any) => {
+    throw new Error("Not implemented");
+  },
+  put: async (url: string, body: any) => {
+    throw new Error("Not implemented");
+  },
+  delete: async (url: string) => {
+    throw new Error("Not implemented");
+  },
 };
 
 export default httpClientPlugin;

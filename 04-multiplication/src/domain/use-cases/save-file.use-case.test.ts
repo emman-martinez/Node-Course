ff9cd1ce6bb5065f9895209a1cef91474bc51c27
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-03-04 15:43:13 -0600

    96. Pruebas - SaveFile UseCase custom values

--- 04-multiplication/src/domain/use-cases/save-file.use-case.test.ts
+++ 04-multiplication/src/domain/use-cases/save-file.use-case.test.ts
@@ -4,15 +4,29 @@ import { SaveFile } from "./save-file.use-case";
 describe("SaveFileUseCase", () => {
   beforeEach(() => {
     // Clean up the outputs directory before each test
-    if (fs.existsSync("outputs")) {
+    const outFolderExists = fs.existsSync("outputs");
+    const customOutFolderExists = fs.existsSync("custom-outputs");
+
+    if (outFolderExists) {
       fs.rmSync("outputs", { recursive: true, force: true });
+    }
+
+    if (customOutFolderExists) {
+      fs.rmSync("custom-outputs", { recursive: true, force: true });
     }
   });
 
   afterEach(() => {
     // Clean up the outputs directory after each test
-    if (fs.existsSync("outputs")) {
+    const outFolderExists = fs.existsSync("outputs");
+    const customOutFolderExists = fs.existsSync("custom-outputs");
+
+    if (outFolderExists) {
       fs.rmSync("outputs", { recursive: true, force: true });
+    }
+
+    if (customOutFolderExists) {
+      fs.rmSync("custom-outputs", { recursive: true, force: true });
     }
   });
 
@@ -23,6 +37,23 @@ describe("SaveFileUseCase", () => {
       fileContent: "Hello, World!",
     };
 
+    const result = saveFile.execute(options);
+    const fileExists = fs.existsSync(filePath);
+    const fileContent = fs.readFileSync(filePath, { encoding: "utf-8" });
+
+    expect(result).toBe(true);
+    expect(fileExists).toBe(true);
+    expect(fileContent).toBe(options.fileContent);
+  });
+
+  test("should save file with custom values", () => {
+    const options = {
+      fileContent: "Custom Content",
+      fileDestination: "custom-outputs/file-destination",
+      fileName: "custom-table-name",
+    };
+    const saveFile = new SaveFile();
+    const filePath = `${options.fileDestination}/${options.fileName}.txt`;
     const result = saveFile.execute(options);
     const fileExists = fs.existsSync(filePath);
     const fileContent = fs.readFileSync(filePath, { encoding: "utf-8" });

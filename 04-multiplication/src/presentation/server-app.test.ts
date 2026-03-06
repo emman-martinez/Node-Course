cc3471bcde7a01ff24945555be7b3409416247f9
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-03-06 15:38:37 -0600

    102. ServerApp - Pruebas unitarias

--- 04-multiplication/src/presentation/server-app.test.ts
+++ 04-multiplication/src/presentation/server-app.test.ts
@@ -11,6 +11,10 @@ describe("Server App", () => {
     name: "test-filename",
   };
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("should create ServerApp instance", () => {
     const serverApp = new ServerApp();
 
@@ -19,31 +23,55 @@ describe("Server App", () => {
   });
 
   test("should run ServerApp with options", () => {
-    const logSpy = jest.spyOn(console, "log");
-    const createTableSpy = jest.spyOn(CreateTable.prototype, "execute");
-    const saveFileSpy = jest.spyOn(SaveFile.prototype, "execute");
+    // const logSpy = jest.spyOn(console, "log");
+    // const createTableSpy = jest.spyOn(CreateTable.prototype, "execute");
+    // const saveFileSpy = jest.spyOn(SaveFile.prototype, "execute");
+    // ServerApp.run(options);
+    // expect(logSpy).toHaveBeenCalledTimes(2);
+    // expect(logSpy).toHaveBeenCalledWith("ServerApp is running...");
+    // expect(logSpy).toHaveBeenCalledWith(
+    //   `File "${options.name}.txt" has been created in the "${options.destination}" directory.`,
+    // );
+    // expect(createTableSpy).toHaveBeenCalledTimes(1);
+    // expect(createTableSpy).toHaveBeenCalledWith({
+    //   base: options.base,
+    //   limit: options.limit,
+    // });
+    // expect(saveFileSpy).toHaveBeenCalledTimes(1);
+    // expect(saveFileSpy).toHaveBeenCalledWith({
+    //   fileContent: expect.any(String),
+    //   fileDestination: options.destination,
+    //   fileName: options.name,
+    // });
+    // logSpy.mockRestore();
+  });
+
+  test("should run with custom values mocked", () => {
+    const logMock = jest.fn();
+    const logErrorMock = jest.fn();
+    const createMock = jest.fn().mockReturnValue("1 x 2 = 2");
+    const saveFileMock = jest.fn().mockReturnValue(true);
+
+    console.log = logMock;
+    console.error = logErrorMock;
+    CreateTable.prototype.execute = createMock;
+    SaveFile.prototype.execute = saveFileMock;
 
     ServerApp.run(options);
 
-    expect(logSpy).toHaveBeenCalledTimes(2);
-    expect(logSpy).toHaveBeenCalledWith("ServerApp is running...");
-    expect(logSpy).toHaveBeenCalledWith(
-      `File "${options.name}.txt" has been created in the "${options.destination}" directory.`,
-    );
-
-    expect(createTableSpy).toHaveBeenCalledTimes(1);
-    expect(createTableSpy).toHaveBeenCalledWith({
+    expect(logMock).toHaveBeenCalledWith("ServerApp is running...");
+    expect(createMock).toHaveBeenCalledWith({
       base: options.base,
       limit: options.limit,
     });
-
-    expect(saveFileSpy).toHaveBeenCalledTimes(1);
-    expect(saveFileSpy).toHaveBeenCalledWith({
+    expect(saveFileMock).toHaveBeenCalledWith({
       fileContent: expect.any(String),
       fileDestination: options.destination,
       fileName: options.name,
     });
-
-    logSpy.mockRestore();
+    expect(logMock).toHaveBeenCalledWith(
+      `File "${options.name}.txt" has been created in the "${options.destination}" directory.`,
+    );
+    expect(logErrorMock).not.toHaveBeenCalled();
   });
 });

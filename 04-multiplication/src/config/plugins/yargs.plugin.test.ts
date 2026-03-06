71f0e1e1cca3ecb04fa4fbee3ef87fb642380d96
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-03-06 14:11:48 -0600

    98. Pruebas - Argv

---
+++ 04-multiplication/src/config/plugins/yargs.plugin.test.ts
@@ -0,0 +1,23 @@
+const runCommand = async (args: string[]) => {
+  process.argv = [...process.argv, ...args];
+
+  const { yarg } = await import("./yargs.plugin");
+
+  return yarg;
+};
+
+describe("Test yargs.plugin.ts", () => {
+  test("should return default values", async () => {
+    const argv = await runCommand(["-b", "5"]);
+
+    expect(argv).toEqual(
+      expect.objectContaining({
+        b: 5,
+        l: 10,
+        s: false,
+        n: "multiplication-table",
+        d: "./outputs",
+      }),
+    );
+  });
+});

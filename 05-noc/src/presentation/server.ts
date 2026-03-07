bbee9777c3a461ae92f9ffe2e27932554b4acdd5
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-03-06 18:09:53 -0600

    111. CRON Tasks

--- 05-noc/src/presentation/server.ts
+++ 05-noc/src/presentation/server.ts
@@ -1,8 +1,20 @@
+import { CronJob } from "cron";
+
 class Server {
   // public: accessible from anywhere
   // static: belongs to the class, not an instance of the class
   public static start() {
     console.log("Server started");
+
+    const job = new CronJob(
+      "*/3 * * * * *", // cronTime
+      () => {
+        const date = new Date();
+        console.log(`3 seconds ${date}`);
+      },
+    );
+
+    job.start();
   }
 }
 

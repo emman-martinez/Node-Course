41184c27c8f48aa3bbc6f21d361814408d358946
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-03-11 17:18:57 -0600

    156. Grabar Logs en Mongo

--- 05-noc/src/presentation/server.ts
+++ 05-noc/src/presentation/server.ts
@@ -1,18 +1,22 @@
-// import { CheckService } from "../domain/use-cases/check/check-service";
+import { CheckService } from "../domain/use-cases/check/check-service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log-repository.impl";
-// import CronService from "./cron/cron-service";
+import CronService from "./cron/cron-service";
 import { EmailService } from "./email/email-service";
+import { MongoLogDataSource } from "../infrastructure/datasources/mongo-log.datasource";
+import { LogSeverityLevel } from "../domain/entities/log.entity";
 
 const emailService = new EmailService();
 const fileSystemDataSource = new FileSystemDataSource();
-const fileSystemLogRepository = new LogRepositoryImpl(fileSystemDataSource);
+const mongoDataSource = new MongoLogDataSource();
+const dataSources = { fileSystemDataSource, mongoDataSource };
+const logRepoository = new LogRepositoryImpl(dataSources.fileSystemDataSource);
 
 class Server {
   // public: accessible from anywhere
   // static: belongs to the class, not an instance of the class
-  public static start() {
+  public static async start() {
     console.log("Server started");
 
     // Send Email
@@ -30,6 +34,9 @@ class Server {
     // });
     // emailService.sendEmailWithFileSystemLogs("emasesosos@gmail.com");
 
+    const logs = await logRepoository.getLogs(LogSeverityLevel.medium);
+    console.log(logs);
+
     // const successCallback = (url: string) => {
     //   console.log(`Successfully checked URL: ${url}`);
     // };
@@ -39,18 +46,16 @@ class Server {
     // };
 
     // CronService.createJob("*/5 * * * * *", () => {
-    //   const url = "https://www.google.com";
+    //   const url = "https://www.googledzfgdfgdfg.com";
     //   const handleSuccessCallback = () => successCallback(url);
 
     //   const checkService = new CheckService(
-    //     fileSystemLogRepository,
+    //     logRepoository,
     //     handleSuccessCallback,
     //     errorCallback,
     //   );
 
     //   checkService.execute(url);
-
-    //   // new CheckService(successCallback, errorCallback).execute("http://localhost:3000");
     // });
   }
 }

644bc546e17a3501b69d92d5d115b933cc9b8ba7
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-03-11 17:03:44 -0600

    155. MongoLogDatasource

--- 05-noc/src/app.ts
+++ 05-noc/src/app.ts
@@ -1,6 +1,5 @@
 import { envs } from "./config/plugins/envs.plugin";
 import { MongoDatabase } from "./data/mongo";
-import LogModel from "./data/mongo/models/log.model";
 import Server from "./presentation/server";
 
 (async () => {
@@ -13,18 +12,5 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
   });
 
-  // Create a collection = tables, document = record
-  // const newLog = await LogModel.create({
-  //   level: "low",
-  //   message: "Test message from Mongo",
-  //   origin: "App.ts",
-  // });
-
-  // await newLog.save();
-  // console.log("New log created:", newLog);
-
-  const logs = await LogModel.find();
-  console.log("All logs:", logs);
-
-  // Server.start();
+  Server.start();
 }

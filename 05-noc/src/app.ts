9814ed2b75f45f485b56de7e3e67291bdd10aa5b
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-03-11 12:35:19 -0600

    154. Crear y leer de Mongo

--- 05-noc/src/app.ts
+++ 05-noc/src/app.ts
@@ -1,5 +1,6 @@
 import { envs } from "./config/plugins/envs.plugin";
 import { MongoDatabase } from "./data/mongo";
+import LogModel from "./data/mongo/models/log.model";
 import Server from "./presentation/server";
 
 (async () => {
@@ -11,6 +12,19 @@ async function main() {
     dbName: envs.MONGO_DB_NAME,
     mongoUrl: envs.MONGO_URL,
   });
+
+  // Create a collection = tables, document = record
+  // const newLog = await LogModel.create({
+  //   level: "low",
+  //   message: "Test message from Mongo",
+  //   origin: "App.ts",
+  // });
+
+  // await newLog.save();
+  // console.log("New log created:", newLog);
+
+  const logs = await LogModel.find();
+  console.log("All logs:", logs);
 
   // Server.start();
 }

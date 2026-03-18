ac9ef55daef3948e9c11b0b7d770301be3e862d4
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-03-18 14:56:52 -0600

    197. Subir repositorio a GitHub-v2

--- 07-restweb/README.md
+++ 07-restweb/README.md
@@ -1,1 +1,5 @@
 # node-webrest-server
+
+# Dev
+
+1. Clone .env.template and create .env
--- 07-restweb/dist/presentation/server.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Server {
-    private app;
-    private folder;
-    private port;
-    constructor(folder: string, port: number);
-    start(): Promise<void>;
-}
-//# sourceMappingURL=server.d.ts.map

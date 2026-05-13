8cec6eec89d5acf25f65902f35f052ec9f680fd7
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-05-12 20:55:11 -0600

    feat: 266. AuthService

--- 08-user-store/src/presentation/auth/controller.ts
+++ 08-user-store/src/presentation/auth/controller.ts
@@ -1,12 +1,20 @@
 import type { Request, Response } from 'express';
+import { RegisterUserDto } from '../../domain';
+import { AuthService } from '../services/auth.service';
 
 export class AuthController {
   // DI
-  constructor() {}
+  constructor(
+    public readonly authService: AuthService
+  ) {}
 
   registerUser = (req: Request, res: Response) => {
-    // Logic for registering a user
-    res.json('User registered successfully');
+    const [error, registerDto] = RegisterUserDto.create(req.body);
+
+    if (error) return res.status(400).json({ error });
+
+    this.authService.registerUser(registerDto!)
+      .then((user) => res.json(user))
   }
 
   loginUser = (req: Request, res: Response) => {

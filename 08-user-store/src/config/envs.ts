69e78d3e9568147613ac1a0c1e9ed482d6fd6415
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-05-13 16:15:51 -0600

    feat: 273. JWT Seed

--- 08-user-store/src/config/envs.ts
+++ 08-user-store/src/config/envs.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export const envs = {
+  JWT_SEED: get('JWT_SEED').required().asString(),
   PORT: get('PORT').required().asPortNumber(),
   MONGO_URL: get('MONGO_URL').required().asString(),
   MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
--- 08-user-store/src/config/jwt.adapter.ts
+++ 08-user-store/src/config/jwt.adapter.ts
@@ -1,4 +1,7 @@
 import jwt from 'jsonwebtoken';
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
 
 export class JwtAdapter {
   static async generateToken(
@@ -6,7 +9,7 @@ export class JwtAdapter {
     duration: jwt.SignOptions['expiresIn'] = '2h'
   ): Promise<string | null> {
     return new Promise((resolve) => {
-      jwt.sign(payload, 'SEED', { expiresIn: duration }, (err, token) => {
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
         if (err) return resolve(null);
         resolve(token!);
       });

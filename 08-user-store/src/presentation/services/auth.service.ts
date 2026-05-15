1122ee91787129029abd11a753423a14dfe9068b
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-05-15 09:07:00 -0600

    feat: 280. Enviar correo electrónico con link de verificación

--- 08-user-store/src/presentation/services/auth.service.ts
+++ 08-user-store/src/presentation/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { bcryptAdapter, JwtAdapter } from '../../config';
+import { bcryptAdapter, envs, JwtAdapter } from '../../config';
 import { UserModel } from '../../data';
 import {
   CustomError,
@@ -6,9 +6,12 @@ import {
   RegisterUserDto,
   UserEntity,
 } from '../../domain';
+import { EmailService } from './email.service';
 
 export class AuthService {
-  constructor() {}
+  constructor(
+    private readonly emailService: EmailService,
+  ) {}
 
   private sanitizeUser(user: UserEntity) {
     const safeUser = { ...user };
@@ -28,12 +31,13 @@ export class AuthService {
       user.password = bcryptAdapter.hash(registerUserDto.password);
       await user.save();
 
-      // JWT <---- to maintain the user authentication
-
       // Send email to validate the email
+      await this.sendEmailValidationLink(user.email);
 
       const userEntity = this.sanitizeUser(UserEntity.fromObject(user));
       const payload = { id: user.id };
+
+       // JWT <---- to maintain the user authentication
       const token = await JwtAdapter.generateToken(payload);
 
       if (!token) throw CustomError.internalServer('Error generating token');
@@ -69,5 +73,27 @@ export class AuthService {
       user: userEntity,
       token,
     };
+  }
+
+  private sendEmailValidationLink = async(email: string) => {
+    const token = await JwtAdapter.generateToken({ email }); // Token expires in 1 hour
+    if (!token) throw CustomError.internalServer('Error generating token');
+
+    const link = `${envs.WEBSERVICE_URL}/auth/validate-email/${token}`;
+    const html = `
+      <h1>Validate your email</h1>
+      <p>Please click the link below to validate your email:</p>
+      <a href="${link}">Validate Email: ${email}</a>
+    `;
+    const options = {
+      to: email,
+      subject: 'Validate your email',
+      htmlBody: html,
+    }
+
+    const isSent = await this.emailService.sendEmail(options);
+    if(!isSent) throw CustomError.internalServer('Error sending email');
+
+    return true;
   }
 }

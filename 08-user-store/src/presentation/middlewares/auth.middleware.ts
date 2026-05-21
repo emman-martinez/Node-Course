11b4f2d750ecbba0f48efc51b3f1df25e5630fb8
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-05-21 15:10:16 -0600

    feat: 293. AuthMiddleware - Proteger Rutas

---
+++ 08-user-store/src/presentation/middlewares/auth.middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+
+export class AuthMiddleware {
+  static async validateJWT(req: Request, res: Response, next: NextFunction) {
+    const authorization = req.header('Authorization');
+
+    if (!authorization)
+      return res.status(401).json({ error: 'No token provided' });
+    if (!authorization.startsWith('Bearer '))
+      return res.status(401).json({ error: 'Invalid bearer token format' });
+
+    const token = authorization.split(' ').at(1) || '';
+
+    try {
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}

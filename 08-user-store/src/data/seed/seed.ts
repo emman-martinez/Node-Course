d3a9dfe20313a74fb88470b9fb8bf0884df77d2e
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-05-26 18:06:54 -0600

    feat: 311. Llenar base de datos

--- 08-user-store/src/data/seed/seed.ts
+++ 08-user-store/src/data/seed/seed.ts
@@ -1,5 +1,11 @@
 import { envs } from '../../config';
-import { MongoDatabase } from '../mongo/mongo-database';
+import {
+  CategoryModel,
+  MongoDatabase,
+  ProductModel,
+  UserModel,
+} from '../mongo';
+import { seedData } from './data';
 
 (async () => {
   await MongoDatabase.connect({
@@ -12,4 +18,38 @@ import { MongoDatabase } from '../mongo/mongo-database';
   await MongoDatabase.disconnect();
 })();
 
-async function main() {}
+const randomBetween0AndX = (x: number) => Math.floor(Math.random() * x);
+
+async function main() {
+  // 0. Clear all
+  await Promise.all([
+    UserModel.deleteMany(),
+    CategoryModel.deleteMany(),
+    ProductModel.deleteMany(),
+  ]);
+
+  // 1. Create users
+  const users = await UserModel.insertMany(seedData.users);
+
+  // 2. Create categories
+  const categories = await CategoryModel.insertMany(
+    seedData.categories.map((category) => {
+      return {
+        ...category,
+        user: users[0]._id, // assign all categories to the first user
+      };
+    })
+  );
+
+  // 3. Create products
+  const products = await ProductModel.insertMany(
+    seedData.products.map((product) => {
+      return {
+        ...product,
+        user: users[randomBetween0AndX(seedData.users.length - 1)]._id, // assign a random user to each product
+        category:
+          categories[randomBetween0AndX(seedData.categories.length - 1)]._id, // assign a random category to each product
+      };
+    })
+  );
+}

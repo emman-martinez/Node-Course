c5c18b4a3c2ca2742c9502f6a6ce844cade6c116
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-06-05 14:28:36 -0600

    feat: WS - Websocket Library

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connected");
+
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+
+  ws.send("Hello from WebSocket server!");
+
+  setInterval(() => {
+    ws.send("Current time: " + new Date().toLocaleTimeString());
+  }, 2000);
+});
+
+console.log("Server is running on ws://localhost:3000");

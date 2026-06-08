fa8e54b16bf78698518fd98b7b221cd4f80a43b2
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-06-08 15:11:37 -0600

    feat: 364. Server Broadcast

--- 11-websockets/src/app.ts
+++ 11-websockets/src/app.ts
@@ -8,12 +8,16 @@ wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
-    console.log("From the client: %s", data);
-    const payload = {
+    const payload = JSON.stringify({
       type: "custom-message",
       payload: data.toString(),
-    };
-    ws.send(JSON.stringify(payload));
+    });
+    // ws.send(JSON.stringify(payload));
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
   });
 
   ws.send("Hello from WebSocket server!");

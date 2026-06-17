7b05c65cad7a2e77c6b06e841541ec0f7c0e3456
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-06-16 18:48:10 -0600

    feat: 375. Endpoints REST para nuestra aplicación

---
+++ 13-socket-server/src/presentation/tickets/controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+
+export class TicketController {
+  // DI - WssService
+  constructor() {}
+
+  public getTickets = async (req: Request, res: Response) => {
+    res.json("getTickets");
+  };
+
+  public getLastTicketNumber = async (req: Request, res: Response) => {
+    res.json("getLastTicketNumber");
+  };
+
+  public pendingTickets = async (req: Request, res: Response) => {
+    res.json("getPendingTickets");
+  };
+
+  public createTicket = async (req: Request, res: Response) => {
+    res.json("createTicket");
+  };
+
+  public drawTicket = async (req: Request, res: Response) => {
+    res.json("drawTicket");
+  };
+
+  public ticketFinished = async (req: Request, res: Response) => {
+    res.json("ticketFinished");
+  };
+
+  public workingOn = async (req: Request, res: Response) => {
+    res.json("workingOn");
+  };
+}
